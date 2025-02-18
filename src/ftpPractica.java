16ccf6ed950310f2f332c9e6892ae0b0a95f6598
Raúl Marrupe <raul.marrupe1@educa.madrid.org>
2025-02-18 12:36:23 +0100

    Descarga de archivos completada

--- src/ftpPractica.java
+++ src/ftpPractica.java
@@ -1,11 +1,18 @@
 
 import org.apache.commons.net.ftp.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class ftpPractica {
 
+
+    static Scanner sc = new Scanner(System.in);
     public static boolean anonymousMode = false;
 
     public static boolean handleValidIP(String input) {
@@ -20,38 +27,82 @@ public class ftpPractica {
         return foundIP || foundURL;
     }
 
-    public static String getFTPServerFromSTDIn(Scanner sc) {
+    public static String getFTPServerFromSTDIn() {
         System.out.println("¿A qué IP o Host quieres conectarte?");
         return sc.nextLine();
     }
 
-    public static String loopUntilValidURL(Scanner sc) {
+    public static String loopUntilValidURL() {
         String output;
-        while (!handleValidIP(output = getFTPServerFromSTDIn(sc))) {
+        while (!handleValidIP(output = getFTPServerFromSTDIn())) {
             System.out.println("No se ha encontrado una dirección válida, inténtalo otra vez");
         }
-        ;
         return output;
     }
 
-    public static int actionMenu(Scanner sc) {
-        System.out.println("Escribe el número de la acción a realizar");
-        System.out.println("1. Listar archivos");
-        System.out.println("2. Descargar archivo");
-        System.out.println("3. Subir archivo");
-        System.out.println("0. Salir");
-        return sc.nextInt();
+    private static String getNewFilePath() throws IOException {
+        File file1 = new File("temp");
+        File file2 = new File(file1.getCanonicalPath());
+        String path = file2.getParentFile().getCanonicalPath();
+        file1.delete();
+        System.out.println("¿Dónde quieres guardar el archivo? (Por defecto: " + path + "/): ");
+        System.out.println("Escrito como /ruta/al/directorio/");
+        sc.nextLine();
+        String newPath = sc.nextLine();
+        if ((new File(newPath).isDirectory())) {
+            return newPath;
+        } else return path;
     }
 
-    public static void doAction(Scanner sc, FTPClient client) throws IOException {
-        switch (actionMenu(sc)) {
-            case 1: listFilesInFTPClient(client);
+    private static int downloadFile(FTPClient client) throws IOException {
+        String pathOutput = getNewFilePath();
+        System.out.println("Di el nombre del archivo que quieres extraer del remoto");
+        String fileName = sc.nextLine();
+        FileOutputStream fos = new FileOutputStream(pathOutput + fileName);
+        client.retrieveFile(fileName, fos);
+        return client.getReplyCode();
+    }
+
+    private static void uploadFile() {
+
+    }
+
+    private static void loopActionMenu(FTPClient client) throws IOException {
+
+        int indexSelected = 99;
+        while (indexSelected!=0) {
+            System.out.println("Escribe el número de la acción a realizar");
+            System.out.println("1. Listar archivos");
+            System.out.println("2. Descargar archivo");
+            if (!anonymousMode) {
+                System.out.println("3. Subir archivo");
+            }
+            System.out.println("0. Salir");
+            doAction(indexSelected = sc.nextInt(), client);
         }
     }
 
-    public static boolean connectToFTPServer(Scanner sc, FTPClient client) {
+    private static boolean actionSucceededForFTP(int code) {
+        return !(code - 499 > 0);
+    }
+
+    private static void doAction(int indexSelected, FTPClient client) throws IOException {
+        switch (indexSelected) {
+            case 1: listFilesInFTPClient(client); break;
+            case 2: downloadFile(client); break;
+            case 3: if (!anonymousMode) {
+                uploadFile(); break;
+            }
+            case 0:
+                System.out.println("Saliendo del programa"); break;
+            default:
+                System.out.println("Índice incorrecto, intentalo de nuevo");
+        }
+    }
+
+    public static boolean connectToFTPServer(FTPClient client) {
         //Loop
-        String FTPServer = loopUntilValidURL(sc);
+        String FTPServer = loopUntilValidURL();
         //Overridden for testing purposes
         FTPServer = "192.168.146.143";
         try {
@@ -66,13 +117,12 @@ public class ftpPractica {
 
     public static void main(String[] args) throws IOException {
         FTPClient client = new FTPClient();
-        Scanner sc = new Scanner(System.in);
 
-        while (!connectToFTPServer(sc, client)) ;
+        while (!connectToFTPServer(client)) ;
         client.enterLocalPassiveMode();
 
         try {
-            while ((attemptLogin(client, sc)) - 499 > 0) {
+            while ((attemptLogin(client)) - 499 > 0) {
                 System.out.println("Error conectándose al servidor, vuelve a intentarlo");
             }
         } catch (IOException e) {
@@ -81,10 +131,11 @@ public class ftpPractica {
             return;
         }
 
-        doAction(sc, client);
+        loopActionMenu(client);
 
+        //loop finished
+        sc.close();
         client.disconnect();
-        System.out.println("fin de la conexión");
     }
 
 
@@ -102,7 +153,7 @@ public class ftpPractica {
         }
 
     }
-    public static int attemptLogin(FTPClient client, Scanner sc) throws IOException {
+    public static int attemptLogin(FTPClient client) throws IOException {
         System.out.print("Username: (dejar en blanco para anónimo) ");
         String password;
         String username = sc.nextLine();
